902716ac700cb5545345d1a9240bdbc27adf0fc7
19127324 <64062447+19127324@users.noreply.github.com>
2022-07-08 10:50:45 +0700

    add menu, bottom sheet Chapter Detail

--- Mangashare/app/src/main/java/com/khtn/mangashare/chapterDetail/ChapterDetailActivity.kt
+++ Mangashare/app/src/main/java/com/khtn/mangashare/chapterDetail/ChapterDetailActivity.kt
@@ -1,23 +1,23 @@
 package com.khtn.mangashare.chapterDetail
 
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
-import android.view.View
-import android.view.animation.Animation
-import android.view.animation.AnimationUtils
-import android.widget.Button
 import android.widget.ImageView
 import android.widget.LinearLayout
-import android.widget.ScrollView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.view.menu.MenuBuilder
 import androidx.appcompat.widget.Toolbar
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.khtn.mangashare.R
+import com.khtn.mangashare.model.chapterItem
 import com.khtn.mangashare.model.comicItem
 
 
@@ -35,17 +35,20 @@ class ChapterDetailActivity : AppCompatActivity() {
     lateinit var backBTN: ImageView
     lateinit var nextBTN: ImageView
     lateinit var viewPager: ViewPager2
+    lateinit var chapterListLL: LinearLayout
 
     private fun init() {
         val intent = intent
         comic = intent.getSerializableExtra("comic") as comicItem
         val num: Int = intent.getIntExtra("chapterNumber", -1)
+
         markLL = findViewById(R.id.markChapterDetailLL)
         markIV = findViewById(R.id.markChapterIM)
-
+        chapterListLL = findViewById(R.id.chooseChapterDetailLL)
         val tblayout = findViewById<ConstraintLayout>(R.id.chapterDetailToolbarCL)
         val nalayout = findViewById<ConstraintLayout>(R.id.bottomChapterDetailCL)
         var tb = findViewById<Toolbar>(R.id.chapterDetailTB)
+
         setSupportActionBar(tb)
         tb.setTitleTextAppearance(this, R.style.ToolbarFont)
         tb.title = "Chương ${num + 1}"
@@ -66,7 +69,6 @@ class ChapterDetailActivity : AppCompatActivity() {
             viewPager.setCurrentItem(num)
         }
 
-
         //back and previous chapter
         backBTN = findViewById(R.id.goPreviousChapterIM)
         nextBTN = findViewById(R.id.goNextChapterIM)
@@ -82,9 +84,12 @@ class ChapterDetailActivity : AppCompatActivity() {
             }
         })
 
-
+        chapterListLL.setOnClickListener {
+            showBottomSheetDialog(comic.chapter)
+        }
     }
-    private fun goBackNextChapter(pos: Int){
+
+    private fun goBackNextChapter(pos: Int) {
         backBTN.setOnClickListener {
             viewPager.setCurrentItem(pos - 1)
         }
@@ -92,6 +97,7 @@ class ChapterDetailActivity : AppCompatActivity() {
             viewPager.setCurrentItem(pos + 1)
         }
     }
+
     private fun markComic(pos: Int) {
         if (comic.chapter[pos].bookmark == true) {
             markIV.setImageResource(R.drawable.ic_unbook_mark)
@@ -114,21 +120,45 @@ class ChapterDetailActivity : AppCompatActivity() {
         }
     }
 
+    @SuppressLint("RestrictedApi")
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        if (menu is MenuBuilder) {
+            menu.setOptionalIconsVisible(true)
+        }
         menuInflater.inflate(R.menu.menu_chapter_detail, menu)
-        return true
+        return super.onCreateOptionsMenu(menu)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         // Handle item selection
         return when (item.getItemId()) {
-            R.id.action_add -> {
+            R.id.action_follow -> {
+                item.setIcon(R.drawable.ic_hotspot_checked)
                 true
             }
-            R.id.action_settings -> {
+            R.id.action_report -> {
+                true
+            }
+            R.id.action_share -> {
                 true
             }
             else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun showBottomSheetDialog(chapterList: ArrayList<chapterItem>) {
+        val bottomSheetDialog = BottomSheetDialog(this)
+        bottomSheetDialog.setContentView(R.layout.fragment_bottom_sheet_chapter_list)
+        val rc = bottomSheetDialog.findViewById<RecyclerView>(R.id.bottomSheetChapterListRC)
+        var adapter = BottomSheetChapterListAdapter(this, chapterList)
+        rc?.layoutManager = LinearLayoutManager(this)
+        rc?.adapter = adapter
+        bottomSheetDialog.show()
+        adapter?.onItemClick = { tmp ->
+            val intent = Intent(this, ChapterDetailActivity::class.java)
+            intent.putExtra("comic", comic)
+            intent.putExtra("chapterNumber", tmp.number?.minus(1) )
+            startActivityForResult(intent, 111)
         }
     }
 }
